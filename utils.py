acb0dede94a89fb8974b6a7d5e115eaa6ae57d9a
Gaurav Agarwal <gaurav.agarwal.mail.09@gmail.com>
2024-02-25 09:08:29 +0530

    edited config and utils

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import face_recognition
+# import face_recognition
 import cv2
 import numpy as np
 import os, datetime
@@ -10,4 +10,8 @@ IMAGE_DB_PATH="./images/"
 def get_camera_details () -> tuple :
     with open('config.json') as f:
         data = json.load(f)
-    return ((i, data[str(i)]["mode_of_operation"], data[str(i)]["camera_id"], data[str(i)]["class_id"]) for i in range(len(data))) 
+
+    return (
+        data["class_id"],
+        data["cameras"]
+    )
